57824ebc69de3ba35e8405973607173bc9b43e83
root <root@fd61d475d76f>
2017-01-04 02:10:15 +0000

    set user defined env vars in /etc/environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in PYTHONHOME PYTHONPATH TOKENURL USERURL USERLISTURL CLIENT_ID CLIENT_SECRET SCOPE; do
-  echo $i=${!i} >>/etc/environment
+  echo $i="'${!i}'" >>/etc/environment
 done
 
 /usr/sbin/sshd -D
